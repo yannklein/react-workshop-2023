09ae688793e352582dfe1c803b9ca5db197074f8
Yann Klein <yann.klein@me.com>
2020-03-19 14:19:58 +0900

    yarn setting update

--- ref/node_modules/eslint-config-airbnb/rules/react.js
+++ ref/node_modules/eslint-config-airbnb/rules/react.js
@@ -1,3 +1,8 @@
+const assign = require('object.assign');
+const baseStyleRules = require('eslint-config-airbnb-base/rules/style').rules;
+
+const dangleRules = baseStyleRules['no-underscore-dangle'];
+
 module.exports = {
   plugins: [
     'react',
@@ -12,6 +17,10 @@ module.exports = {
   // View link below for react rules documentation
   // https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
   rules: {
+    'no-underscore-dangle': [dangleRules[0], assign({}, dangleRules[1], {
+      allow: dangleRules[1].allow.concat(['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']),
+    })],
+
     // Specify whether double or single quotes should be used in JSX attributes
     // https://eslint.org/docs/rules/jsx-quotes
     'jsx-quotes': ['error', 'prefer-double'],
@@ -185,7 +194,7 @@ module.exports = {
 
     // Prevent multiple component definition per file
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
-    'react/no-multi-comp': ['error', { ignoreStateless: true }],
+    'react/no-multi-comp': 'off',
 
     // Prevent usage of setState
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-set-state.md
@@ -451,6 +460,11 @@ module.exports = {
     // Prevent usage of UNSAFE_ methods
     // https://github.com/yannickcr/eslint-plugin-react/blob/157cc932be2cfaa56b3f5b45df6f6d4322a2f660/docs/rules/no-unsafe.md
     'react/no-unsafe': 'off',
+
+    // Enforce shorthand or standard form for React fragments
+    // https://github.com/yannickcr/eslint-plugin-react/blob/bc976b837abeab1dffd90ac6168b746a83fc83cc/docs/rules/jsx-fragments.md
+    // TODO: enable, semver-major
+    'react/jsx-fragments': ['off', 'syntax'],
   },
 
   settings: {
